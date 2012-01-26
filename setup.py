95dcc09d5af787814bf0da985e1afc67d6e5814d
Peter Coles <peter@mrcoles.com>
2012-01-26 01:46:58 -0500

    first stab at making this look like a real project

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='readmd',
+    version='0.0.1',
+    description='Make markdown easier to read as plaintext',
+    author='Peter Coles',
+    author_email='peter@mrcoles.com',
+    url='https://github.com/mrcoles/readmd',
+    packages=['readmd'],
+    scripts=['bin/readmd'],
+    )
